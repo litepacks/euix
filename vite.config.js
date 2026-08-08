eba7e1415f5aa943f49f4e4514ee726e86556f89
crafttcore <crafttcore@gmail.com>
2026-08-08 23:18:33 +0300

    fix: prevent mangling of essential internal properties in vite configurations

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,8 @@ export default defineConfig({
       mangle: {
         toplevel: true,
         properties: {
-          regex: /^_/
+          regex: /^_/,
+          reserved: ['_index', '_parentStateKey', '_targetEl', '_evt']
         }
       },
       format: {
